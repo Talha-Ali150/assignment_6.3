8809ad7d07ca3fe63b0070a9bf7f242f52ec8524
Talha-Ali150 <syed.talha.ali.150@gmail.com>
2022-12-23 05:16:50 +0500

    company settings reset, setCounter

--- src/components/CompanySettings/index.jsx
+++ src/components/CompanySettings/index.jsx
@@ -1,84 +1,135 @@
-import { db } from "../../config/firebase"
-import { doc,setDoc,addDoc, collection, onSnapshot } from "firebase/firestore"
-import { useEffect, useState } from "react"
+import { useState } from "react";
+import { useEffect } from "react";
+import { db } from "../../config/firebase";
+import { doc, updateDoc,getDoc,  } from "firebase/firestore";
 
-export default function CompanySettings(){
-    const [tokenLimit, setTokenLimit] = useState('')
-    const [estimatedTime, setEstimatedTime] = useState('')
-    const [tokenValue, setTokenValue] = useState(0)
+export default function CompanySettings() {
+    const [tokenLimit, setTokenLimit] = useState(0)
+    const [showTokenLimit, setShowTokenLimit] = useState(true)
+    const [tokenCounter, setTokenCounter] = useState(localStorage.getItem('currentToken'))
 
-    const tokenLimitFunc=(e)=>{
-        setTokenLimit(e.target.value)
-        setTokenValue(0)
+    const tokenLimitFunc = (event) => {
+        setTokenLimit(event.target.value)
     }
 
-    const estimatedTimeFunc=(e)=>{
-        setEstimatedTime((e.target.value)*(60000))
-    }
-
-    const updateTokenFunc=()=>{
-        setTokenValue(+(tokenValue)+1)
+    const clickHandle = () => {
+        alert('updated')
+        setShowTokenLimit(false)
     }
 
 
-    const addTokenLimitFirebase=async()=>{
-        try{
-    await addDoc(collection(db, "TokenLimit"),{tokenLimit})
-    alert(`success`)
-}
-    catch(error){
-        console.log(error)
+    const buttonFunc= async ()=>{
+        setTokenCounter(0)
+        updateToken()
+        await updateDoc(doc(db, "companies", localStorage.getItem('compId')), {
+            token: tokenLimit,
+            createdAt : Date.now()
+          });
     }
-}
-
-
-useEffect(() => {
-    onSnapshot(collection(db, "TokenLimit"), (querySnapshot) => {
-
-        querySnapshot.forEach((doc) => {
-            // comList.push({ id: doc.id, ...doc.data() })
-            console.log(doc.data())
-        });
-        // setCompaniesList(comList)
-    })
-}, [])
-
-const sendDetailsFirebase = async()=>{
-    try{
-    await setDoc(doc(db, "tokenDetails", localStorage.getItem('compId')), {
-        currentTokenValue : 12,
-        estimatedTime : 1000,
-        tokenLimit : tokenLimit 
-    })
-alert(`success`)}
-    catch(error){
-        console.log(error)
+    const updateToken= async ()=>{
+        await updateDoc(doc(db, "companies", localStorage.getItem('compId')), {
+            currentToken: tokenCounter
+          });
     }
-}
+
+    const currentTokenValue= async()=>{
+        const docRef = doc(db, "companies", localStorage.getItem('compId'))
+        const docSnap = await getDoc(docRef);
+        let myValue;
+        
+        if (docSnap.exists()) {
+          myValue = (docSnap.data()).currentToken
+
+        } else {
+          console.log("No such document!");
+        }
+        setTokenCounter(myValue)
+        localStorage.setItem('currentToken', myValue)
+    }
+
+    
+    useEffect(() => {
+        const interval = setInterval(async () => {
+                currentTokenValue()
+                updateToken()
+                const docRef = doc(db, "companies", localStorage.getItem('compId'))
+                const docSnap = await getDoc(docRef);
+                let myValue;
+                
+                if (docSnap.exists()) {
+                  myValue = (docSnap.data()).token
+
+                } else {
+                  console.log("No such document!");
+                }
+              
 
 
+            if (tokenCounter<myValue){                
+            setTokenCounter(tokenCounter+1)
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+      }, [tokenCounter]);
 
+      const resetCounter= async()=>{
+        setTokenCounter(0)
+      }
+
+      const giveAlert=async()=>{
+        const docRef = doc(db, "companies", localStorage.getItem('compId'))
+        const docSnap = await getDoc(docRef);
+        let createTime;
+        let estimatedTime;
+        
+        if (docSnap.exists()) {
+          createTime = (docSnap.data()).createdAt
+          estimatedTime = (docSnap.data()).estimatedTime
+
+          if( Math.ceil((Date.now()/1000/60) - (createTime/1000/60)) > estimatedTime )  {
+            alert('abc')
+          }
+          else{
+            alert('xyz')
+          }
+
+        } else {
+          console.log("No such document!");
+        }
+      }
+
+
+      
 
     return (
         <div>
-            <h1>This is company settings page</h1>
-            <p>{localStorage.getItem('compId')}</p>
+            {/* {showTokenLimit && <div>
+                <input value={tokenLimit} type="number" placeholder="Enter Token Limit" onChange={tokenLimitFunc} />
+                <button onClick={clickHandle}>SET</button>
+            </div>}
 
-            <label htmlFor="total-token">total tokens for today:
-                    <input value={tokenLimit} onChange={tokenLimitFunc} type="number" placeholder="Enter limit of tokens" id="total-token" />
-            </label>
-            <button onClick={addTokenLimitFirebase}>DONE</button>
+            {showTokenLimit || <button onClick={() => { setShowTokenLimit(true) }}>SET token Limit</button>}
 
-            {/* <label htmlFor="estimated-time">Enter estimated time for next:
-                    <input value={estimatedTime} onChange={estimatedTimeFunc} type="number" placeholder="Enter time in minutes" id="total-token" />
-            </label>
-            {(tokenValue <= tokenLimit) && <button onClick={updateTokenFunc}>Update Token</button>}
+<div>
+    <p>{tokenCounter}</p>
+    {(tokenCounter < tokenLimit) && <button onClick={()=>{setTokenCounter(tokenCounter+1)}}>Update Token</button>}
+</div> */}
 
-            <button>allow</button>
-            <button>disallow</button> */}
 
-            <button onClick={sendDetailsFirebase}>Add Details to Firebase</button>
+<input type="number"  value={tokenLimit}  onChange={tokenLimitFunc} />
+<button onClick={buttonFunc}> sned firebase</button>
 
-            </div>
-    )
+<h1>Current TOKEN</h1>
+<p>current Toekn is: {tokenCounter}</p>
+
+
+<button>GET DOC</button>
+
+<button onClick={resetCounter}>RSET counter</button>
+
+
+<button onClick={giveAlert}>Alert</button>
+        </div>
+    );
+
 }
