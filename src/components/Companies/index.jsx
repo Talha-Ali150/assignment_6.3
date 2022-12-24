03b07afb274ddb5ba4519c97c3f0fb683f9af1c8
Talha-Ali150 <syed.talha.ali.150@gmail.com>
2022-12-24 05:33:44 +0500

    spinner added

--- src/components/Companies/index.jsx
+++ src/components/Companies/index.jsx
@@ -1,11 +1,28 @@
+import { auth } from "../../config/firebase";
+import {query, where, getDocs } from "firebase/firestore";
+
 import { onSnapshot, collection } from "firebase/firestore";
 import { db } from "../../config/firebase";
+// import { Spin } from "antd";
+import CircularStatic from "../Loader";
 
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 export default function Companies() {
 
+    const fetchUSINGwhere=async()=>{
+        const q = query(collection(db, "companies"), where(`userId`, "==", `${auth.currentUser.uid}`));
+
+        const querySnapshot = await getDocs(q);
+        console.log(querySnapshot)
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(" => ", doc.data());
+        });
+    }
+
     const [companiesList, setCompaniesList] = useState([])
+    const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
     
 const compSettings=async(id)=>{
@@ -13,16 +30,38 @@ const compSettings=async(id)=>{
 localStorage.setItem('compId', id)
 }
 
-    useEffect(() => {
-        onSnapshot(collection(db, "companies"), (querySnapshot) => {
-            const comList = []
+    // useEffect(() => {
+    //     const loadCompanies=()=>{
+    //         try{
+                
+    //     }
+    //     }
+    //     catch(e){
+    //     }
+    //     loadCompanies()
+    // }, [])
 
-            querySnapshot.forEach((doc) => {
-                comList.push({ id: doc.id, ...doc.data() })
-            });
-            setCompaniesList(comList)
-        })
-    }, [])
+    useEffect(() => {
+        const loadCompanies = () => {      
+          try {
+            setLoading(true)
+            onSnapshot(collection(db, "companies"), (querySnapshot) => {
+                const comList = []
+
+                querySnapshot.forEach((doc) => {
+                    comList.push({ id: doc.id, ...doc.data() });
+                })
+                setCompaniesList(comList);
+                setLoading(false)
+            })
+          } catch{       
+            setLoading(true)     
+            console.log('this is eror')
+            setLoading(false)
+          }
+        }
+      loadCompanies()
+      }, [])
 
 
     return (
@@ -30,8 +69,16 @@ localStorage.setItem('compId', id)
             <h1>
                 This is Companies Page</h1>
             <img onClick={() => navigate('/add-company')} src="https://cdn.pixabay.com/photo/2014/04/02/10/55/plus-304947_1280.png" alt="" height={50} width={50} />
-            <h1>Your Companies</h1>
+            <div>
+                            <h1>Your Companies</h1>
+                            <button onClick={fetchUSINGwhere}>Fetc</button>
+            </div>
             <h1>Existing Companies</h1>
+            
+            <div>
+                {loading && <CircularStatic/>}
+            </div>
+
             <div>
                 {companiesList.map(item => {
                     return <> <div className="card" style={{width: '18rem'}}>
@@ -45,6 +92,11 @@ localStorage.setItem('compId', id)
                     </>
                 })}
 
+            </div>
+
+            <div>
+                <h1>USER ID</h1>
+                <p>{auth.currentUser.uid}</p>
             </div>
         </div>
     );
